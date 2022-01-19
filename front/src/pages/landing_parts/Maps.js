f94621d59ecc42d36bd8227bbd7ffb538aee9110
Youngsang Kim <youngsangkim@YoungsasMBP2021.home>
2022-01-19 17:34:28 -0500

    updated google maps

--- front/src/pages/landing_parts/Maps.js
+++ front/src/pages/landing_parts/Maps.js
@@ -1,36 +1,74 @@
-import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import React from 'react';
 import restaurantData from '../../Data/Restaurant.json'
 
-const googleKey = process.env.REACT_APP_GOOGLE_KEY;
+// function Map() {
+//   // const [selectedMarker, setSelectedMarker] = useState(true);
 
-function Map() {
-  // const [selectedMarker, setSelectedMarker] = useState(true);
+//   return (
+//     <div>
+//       <GoogleMap
+//         defaultZoom={16}
+//         defaultCenter={{ lat: restaurantData[0].lat, lng: restaurantData[0].lng }}
+//       >
+//         <Marker 
+//           position={{ lat: restaurantData[0].lat, lng: restaurantData[0].lng }}
+//         />
+//       </GoogleMap>
+//     </div>
+//   );
+// }
 
-  return (
-    <div>
-      <GoogleMap
-        defaultZoom={16}
-        defaultCenter={{ lat: restaurantData[0].lat, lng: restaurantData[0].lng }}
-      >
-        <Marker 
-          position={{ lat: restaurantData[0].lat, lng: restaurantData[0].lng }}
-        />
-      </GoogleMap>
-    </div>
-  );
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "50vh"
 }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 const Maps = (props) => {
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  })
+
+  const [ showInfoWindow, setShowInfoWindow ] = React.useState(true);
+
+  if(loadError) return "Error";
+  if(!isLoaded) return "Loading...";
+
   return (
-    <div className="Maps_Container" style={{ width: "100vw", height: "50vh" }}>
-      <WrappedMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${googleKey}`}
-        loadingElement={<div style={{ height: "100% "}} />}
-        containerElement={<div style={{ height: "100%" }} />}
-        mapElement={<div style={{ height: "100%" }} />}
-      />
+    <div style={{ width: "100vw", height: "50vh "}}>
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={16}
+        center={{ lat: restaurantData[0].lat, lng: restaurantData[0].lng }}
+      >
+        <Marker
+          position={{ lat: restaurantData[0].lat, lng: restaurantData[0].lng }}
+          onClick={()=>{
+            setShowInfoWindow(true)
+          }}
+        />
+        {showInfoWindow ? (
+          <InfoWindow
+            position={{ lat: restaurantData[0].lat+0.0006, lng: restaurantData[0].lng }}
+            onCloseClick={()=> {
+              setShowInfoWindow(false)
+            }}
+          >
+            <div>
+              <h2>
+                SushiVille
+              </h2>
+              <p>{restaurantData[0].address}</p>
+              <a href="https://www.google.com/maps/place/Sushiville/@41.155677,-74.193748,16z/data=!4m8!1m2!3m1!2sSushiville!3m4!1s0x89c2dfc1c127b3e3:0x4735a45026923a62!8m2!3d41.1556049!4d-74.1935692">View on Google Maps</a>
+            </div>
+          </InfoWindow>
+        ) : !showInfoWindow}
+      </GoogleMap>
     </div>
   );
 }
