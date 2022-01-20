6744b47f514c6c4660e163af7a5440921bc36741
Youngsang Kim <youngsangkim@YoungsasMBP2021.home>
2022-01-20 03:18:58 -0500

    formatted menu section

--- front/src/pages/landing_parts/MenuComponents/Menu.js
+++ front/src/pages/landing_parts/MenuComponents/Menu.js
@@ -1,7 +1,4 @@
 import React, { useState } from 'react';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import box from '@material-ui/core/Box';
 
 import ALaCarte from './ALaCarte';
 import Appetizer from './Appetizer';
@@ -14,30 +11,65 @@ import SushiSashimi from './SushiSashimi';
 
 const Menu = (props) => {
 
-  const [menuSelected, setMenuSelected] = useState('sRolls')
+  const [menuSelected, setMenuSelected] = useState(<SpecialRolls />)
+
+  // Handlers
+
+  const sRollHandler = () => {
+    setMenuSelected(<SpecialRolls />)
+  }
+
+  const rRollHandler = () => {
+    setMenuSelected(<RegularRolls />)
+  }
+
+  const lSpecialHandler = () => {
+    setMenuSelected(<LunchSpecial />)
+  }
+
+  const appetizerHandler = () => {
+    setMenuSelected(<Appetizer />)
+  }
+
+  const soupSaladHandler = () => {
+    setMenuSelected(<SoupSalad />)
+  }
+
+  const kitchenEntreeHandler = () => {
+    setMenuSelected(<KitchenEntree />)
+  }
+
+  const sSashimiHandler = () => {
+    setMenuSelected(<SushiSashimi />)
+  }
+
+  const aLaCarteHandler = () => {
+    setMenuSelected(<ALaCarte />)
+  }
 
   return (
     <nav className="Menu_Container">
       <ul>
-        <li>Special Rolls</li>
-        <li>Regular Rolls</li>
-        <li>Lunch Special</li>
-        <li>Appetizer</li>
-        <li>Soup & Salad</li>
-        <li>Kitchen Entree</li>
-        <li>Sushi & Sashimi</li>
-        <li>A La Carte</li>
+        <li onClick={sRollHandler}>Special Rolls</li>
+        <li onClick={rRollHandler}>Regular Rolls</li>
+        <li onClick={lSpecialHandler}>Lunch Special</li>
+        <li onClick={appetizerHandler}>Appetizer</li>
+        <li onClick={soupSaladHandler}>Soup & Salad</li>
+        <li onClick={kitchenEntreeHandler}>Kitchen Entree</li>
+        <li onClick={sSashimiHandler}>Sushi & Sashimi</li>
+        <li onClick={aLaCarteHandler}>A La Carte</li>
       </ul>
       <div className='menuContents'>
         {/* content will be show here */}
-        <SpecialRolls />
+        {menuSelected}
+        {/* <SpecialRolls />
         <RegularRolls />
         <LunchSpecial />
         <Appetizer />
         <SoupSalad />
         <KitchenEntree />
         <SushiSashimi />
-        <ALaCarte />
+        <ALaCarte /> */}
       </div>
     </nav>
   );
